84adad01f937c71059353d3a98c47bce7488c448
rusty-gr <apple9872@googlemail.com>
2012-05-30 19:47:47 +0200

    added the files that were missing in the last commit

---
+++ ServerApp/loginshard/src/main/java/com/gamerevision/gwlpr/loginshard/controllers/Handshake.java
@@ -0,0 +1,17 @@
+/**
+ * For copyright information see the LICENSE document.
+ */
+
+package com.gamerevision.gwlpr.loginshard.controllers;
+
+
+/**
+ * This shardlet handles the handshake process for GW clients.
+ * It establishes an encrypted session.
+ * 
+ * @author _rusty
+ */
+public class Handshake 
+{
+    // TODO
+}
