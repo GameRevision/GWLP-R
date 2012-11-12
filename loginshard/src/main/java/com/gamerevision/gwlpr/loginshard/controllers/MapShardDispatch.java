c369ae2aa49ecfbf6f170a213eea33a5d27795ee
rusty-gr <apple9872@googlemail.com>
2012-11-12 16:39:18 +0100

    added simple map-shard management for login server

--- loginshard/src/main/java/com/gamerevision/gwlpr/loginshard/controllers/MapShardDispatch.java
+++ loginshard/src/main/java/com/gamerevision/gwlpr/loginshard/controllers/MapShardDispatch.java
@@ -10,10 +10,12 @@ import com.gamerevision.gwlpr.actions.loginserver.ctos.P041_CharacterPlayInfoAct
 import com.gamerevision.gwlpr.loginshard.SessionAttachment;
 import com.gamerevision.gwlpr.loginshard.views.DispatcherView;
 import com.realityshard.shardlet.EventHandler;
+import com.realityshard.shardlet.RemoteShardletContext;
 import com.realityshard.shardlet.Session;
 import com.realityshard.shardlet.ShardletContext;
 import com.realityshard.shardlet.utils.GenericShardlet;
 import java.util.HashMap;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,7 +29,18 @@ public class MapShardDispatch extends GenericShardlet
 {
     
     private static Logger LOGGER = LoggerFactory.getLogger(MapShardDispatch.class);
+    
+    private final Map<Integer, RemoteShardletContext> mapShards;
     private DispatcherView dispatcherView;
+    
+    
+    /**
+     * Constructor.
+     */
+    public MapShardDispatch()
+    {
+        mapShards = new HashMap<>();
+    }
     
     
     /**
@@ -71,17 +84,27 @@ public class MapShardDispatch extends GenericShardlet
         params.put("MapId", String.valueOf(mapId));
         
         
-        ShardletContext mapShard = null;
+        RemoteShardletContext mapShard = null;
         
-        try
+        // lets see if we already got that map shard:
+        if (mapShards.containsKey(mapId))
         {
-            LOGGER.debug("Trying to create a map shard");
-            
-            mapShard = getShardletContext().tryCreateGameApp("MapShard", params);
+            mapShard = mapShards.get(mapId);
         }
-        catch (Exception ex)
+        else
         {
-            LOGGER.error("Could not create map shard.", ex);
+            // else create a new one
+            try
+            {
+                LOGGER.debug("Trying to create a map shard");
+
+                mapShard = getShardletContext().tryCreateGameApp("MapShard", params);
+                mapShards.put(mapId, mapShard);
+            }
+            catch (Exception ex)
+            {
+                LOGGER.error("Could not create map shard.", ex);
+            }
         }
         
         // lets check if we were successfull with the game app creation
@@ -95,7 +118,7 @@ public class MapShardDispatch extends GenericShardlet
         int accId = attach.getAccountId();
         int charId = attach.getCharacterId();
         
-        mapShard.sendRemoteEventAction(new ISC_AcceptClientRequestAction(session, 1, 2, accId, charId));
+        mapShard.sendTriggerableAction(new ISC_AcceptClientRequestAction(session, 1, 2, accId, charId));
         
         // note that our second method will be invoked when the map shard replies.
     }
